b5d3d091eaf72a8d8303fb5feff44f3f9ae9bd34
Sreenivasa Reddy <hr.sreenivas16@gmail.com>
2023-10-14 16:40:30 -0500

    added functional compoent syntax

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// React.createElement() ==> react Object is created ==> render() method will create HTMLElement
-const header = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Welcome to Fundations of React!"
-);
-
 // JSX - is not HTM in JS but you can say JSX is XML like syntax
 const jsxheader = <h1 id="jsxheading">Howdy React using JSX</h1>;
 // ABOVE: JSX (transpiled before it reaches the JS)
 //1. JSX is identified by Parcel bundler and handes it to Bable to transpile
 
+// React Functional Component
+const HeaderComponent = () => {
+  return <h2>React Functional Component</h2>;
+};
+
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-reactRoot.render(jsxheader);
+// jsxheader react element is rendered
+//reactRoot.render(jsxheader);
+
+//render functional component
+reactRoot.render(<HeaderComponent />);
