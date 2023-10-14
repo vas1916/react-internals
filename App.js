44f1e64ca5764f44b8d7dd616fc9fcd934515d86
Sreenivasa Reddy <hr.sreenivas16@gmail.com>
2023-10-14 15:59:39 -0500

    first commit of fundation

--- App.js
+++ App.js
@@ -1,2 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// React.createElement() ==> react Object is created ==> render() method will create HTMLElement
+const header = React.createElement("h1", {}, "Welcome to Fundations of React!");
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+reactRoot.render(header);
