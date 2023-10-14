e6d3ae938f8d954e7fb5920a134e2fd598ff08dd
Sreenivasa Reddy <hr.sreenivas16@gmail.com>
2023-10-14 15:48:13 -0500

    commenting code to start foundation Layout

--- App.js
+++ App.js
@@ -1,15 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//creating h3 element using react
-const reactHeading = React.createElement(
-  "h3",
-  { id: "react-heading" },
-  "Hello World using React!"
-);
-
-console.log("@reactHeading is react objectnot <h3> element", reactHeading);
-
-//creating react root
-const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
-//rendering rect element inside react root
-reactRoot.render(reactHeading);
